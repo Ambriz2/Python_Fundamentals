be0bcd2bb0a6ba85e832fb496bcf5b2b9b061146
Ulices Villegas <ulicesvillegas7@gmail.com>
2021-03-09 16:01:47 -0700

    container components react

--- src/components/CampSiteInfoComponent.js
+++ src/components/CampSiteInfoComponent.js
@@ -18,18 +18,17 @@ class CampSiteInfo extends Component {
         )
     }
     render() {
-
         if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
-            return (
-                <div />
-            );
+        return <div />; 
     }
     renderComments(comments){
         if(comments){
